afcca55d3e0c887fafcc6f6d96a4422a08dc3cee
ElkanRoelen <roelen@elkan.nl>
2022-12-08 13:47:10 +0100

    Added  external ip

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@
 
 echo "Starting ProofMe TURN/STUN server"
 EXTERNAL_IP="$(curl -4  https://icanhazip.com 2>/dev/null)"
-turnserver -a -v -L 0.0.0.0 --external-ip="${EXTERNAL_IP}" --server-name "${TURN_SERVER_NAME}" --static-auth-secret="${TURN_SECRET}" --realm=${TURN_REALM}  -p ${TURN_PORT} --min-port ${TURN_PORT_START} --max-port ${TURN_PORT_END} ${TURN_EXTRA}
+echo "Listening on ${EXTERNAL_IP}"
+turnserver -a -v -L $EXTERNAL_IP --external-ip="${EXTERNAL_IP}" --server-name "${TURN_SERVER_NAME}" --static-auth-secret="${TURN_SECRET}" --realm=${TURN_REALM}  -p ${TURN_PORT} --min-port ${TURN_PORT_START} --max-port ${TURN_PORT_END} ${TURN_EXTRA}
